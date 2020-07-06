a8d99a6de4c701cea722048f231a688f1b075f57
Nils Knappmeier <github@knappi.org>
2020-07-06 16:34:02 +0200

    fix: use "win32" as platform name for Windows

--- prepare/lib/required-assets.js
+++ prepare/lib/required-assets.js
@@ -7,8 +7,8 @@ function requiredAssets(release) {
 	return [
 		{ platform: "linux--ia32", assetName: `chisel_${version}_linux_386.gz` },
 		{ platform: "linux--x64", assetName: `chisel_${version}_linux_amd64.gz` },
-		{ platform: "win--ia32", assetName: `chisel_${version}_windows_386.gz` },
-		{ platform: "win--x64", assetName: `chisel_${version}_windows_amd64.gz` },
+		{ platform: "win32--ia32", assetName: `chisel_${version}_windows_386.gz` },
+		{ platform: "win32--x64", assetName: `chisel_${version}_windows_amd64.gz` },
 		{ platform: "darwin--ia32", assetName: `chisel_${version}_darwin_386.gz` },
 		{ platform: "darwin--x64", assetName: `chisel_${version}_darwin_amd64.gz` },
 	];
