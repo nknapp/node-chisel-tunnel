6fdc12fd27046e4b1e5b3d499d66ee85d2dbbd48
Nils Knappmeier <github@knappi.org>
2020-07-06 18:55:08 +0200

    test: use "gz"-file as fixture to ensure same checkout on different os

--- src/lib/file-operations.test.js
+++ src/lib/file-operations.test.js
@@ -1,9 +1,11 @@
 const { sha256 } = require("./file-operations");
-const fixtureFile = require.resolve("../../test-utils/fixtures/fileHashTestFixture.txt");
+const fixtureFile = require.resolve("../../test-utils/fixtures/fileHashTestFixture.txt.gz");
 
-describe("file-hash", () => {
-	it("should compute the sha256 hash of a file", async () => {
-		const hash = await sha256(fixtureFile);
-		expect(hash).toEqual("fc4b5fd6816f75a7c81fc8eaa9499d6a299bd803397166e8c4cf9280b801d62c");
+describe("file-operations", () => {
+	describe('the "sha256"', () => {
+		it("should compute the sha256 hash of a file", async () => {
+			const hash = await sha256(fixtureFile);
+			expect(hash).toEqual("b6f0999a4f59ee8e9249300e859e6fb5ef4da65d9e643ad8e11f892a59bf6215");
+		});
 	});
 });
